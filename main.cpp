167192eae61502d033fdfa3bd946e8b8cf4f5667
Arti <kirnevartem30@gmail.com>
2022-09-06 01:01:52 +0200

    little clean up

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-bool chooseTheDoorTest(bool viewer_switches_the_door)
+bool chooseTheDoorTest(bool b_does_viewer_switches_the_door)
 {
     unsigned short correct_door = random() % 3;
     unsigned short chosen_door = random() % 3;
 
-    bool viewer_had_chosen_correctly = chosen_door == correct_door;
+    bool b_had_viewer_chosen_correctly = chosen_door == correct_door;
 
-    bool had_viewer_won = (viewer_had_chosen_correctly != viewer_switches_the_door);
-    return had_viewer_won;
+    bool b_had_viewer_won = (b_had_viewer_chosen_correctly != b_does_viewer_switches_the_door);
+    return b_had_viewer_won;
 }
 
 unsigned long repeatTestsWithSetSwitch(unsigned number_of_tests, bool b_does_viewer_switch)
@@ -24,10 +24,8 @@ unsigned long repeatTestsWithSetSwitch(unsigned number_of_tests, bool b_does_vie
         //noticeable if you do millions and more tests
         cout << rc << "Try #"<< i+1 << flush;
 
-        n_successes += chooseTheDoorTest(b_does_viewer_switch);
+        n_successes += (int) chooseTheDoorTest(b_does_viewer_switch);
     }
-
-    cout << "\rTest is finished!" << endl;
 
     return n_successes;
 }
@@ -38,17 +36,17 @@ int main()
 
     long long number_of_tests;
     do {
-        cout << "Enter number of repeats for tests\n" << endl;
+        cout << "Enter number of repeats for tests" << endl;
         cin >> number_of_tests;
     } while (number_of_tests < 1);
 
     cout << "Start "<< number_of_tests <<" tests in which viewers switches their door" << endl;
     unsigned n_successes_with_switches = repeatTestsWithSetSwitch(number_of_tests, true);
-    cout << n_successes_with_switches << " of " << number_of_tests << " viewers won by switching the door" << endl;
+    cout << n_successes_with_switches << "\t of " << number_of_tests << " viewers won by switching the door" << endl;
 
     cout << "Start " << number_of_tests << " tests in which viewers don\'t switches their door" << endl;
     unsigned n_successes_without_switches = repeatTestsWithSetSwitch(number_of_tests, false);
-    cout << n_successes_without_switches << " of " << number_of_tests << " viewers won by NOT switching the door" << endl;
+    cout << n_successes_without_switches << "\t of " << number_of_tests << " viewers won by NOT switching the door" << endl;
 
     return 0;
 }
